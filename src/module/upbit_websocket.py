a6264d2cecaa017cb23217d9693f5392763f58d8
kdhur <1kdhur2@gmail.com>
2025-03-12 01:58:18 +0900

    [fix] while -> async for - 조회한 데이터를 main에서 받아서 사용하기 위함

--- src/module/upbit_websocket.py
+++ src/module/upbit_websocket.py
@@ -8,51 +8,15 @@ import ssl
 import certifi
 
 import src.resources.config as cfg
-from src.module.util import send_request, set_loglevel
+from src.module.util import set_loglevel
+from src.module.upbit_rest_api import get_items
 
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
 
-def get_items(market, except_item):
-    try:
-
-        # 조회결과 리턴용
-        rtn_list = []
-
-        # 마켓 데이터
-        markets = market.split(',')
-
-        # 제외 데이터
-        except_items = except_item.split(',')
-
-        url = "/v1/market/all"
-        querystring = {"isDetails": "false"}
-        response = send_request("GET", cfg.UPBIT_URL + url, querystring, "")
-        data = response.json()
-
-        # 조회 마켓만 추출
-        for data_for in data:
-            for market_for in markets:
-                if data_for["market"].split('-')[0] == market_for:
-                    rtn_list.append(data_for)
-
-        # 제외 종목 제거
-        for rtnlist_for in rtn_list[:]:
-            for exceptItemFor in except_items:
-                for marketFor in markets:
-                    if rtnlist_for["market"] == marketFor + '-' + exceptItemFor:
-                        rtn_list.remove(rtnlist_for)
-
-        return rtn_list
-
-    except Exception as e:
-        logging.error("Exception Raised In Websocket Client!")
-        logging.error(e)
-        raise
-
 def get_subscribe_items():
     try:
-        subscribe_items = []
+        subscribe_items = list()
 
         # KRW 마켓 전 종목 추출
         items = get_items("KRW", '')
@@ -97,11 +61,10 @@ async def upbit_ws_client():
 
             await ws.send(subscribe_data)
 
-            while True:
+            async for message in ws:
                 period = datetime.datetime.now()
 
-                data = await ws.recv()
-                data = json.loads(data)
+                data = json.loads(message)
                 logging.info(data)
 
                 # 5초마다 종목 정보 재 조회 후 추가된 종목이 있으면 웹소켓 다시 시작
@@ -124,6 +87,8 @@ async def upbit_ws_client():
                         await ws.close()
                         time.sleep(1)
                         await upbit_ws_client()
+
+                yield data
 
     except Exception as e:
         logging.error("Exception Raised In Websocket Client!")
